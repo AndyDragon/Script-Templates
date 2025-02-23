3947fb54c79e9e9542a15e80cd974eb1cb1b0c22
Andrew Forget <andrew.forget@andydragon.com>
2025-02-22 20:31:27 -0500

    Updated all the page scripts for the new approved script

--- Script Templates/build_compiler.sh
+++ Script Templates/build_compiler.sh
@@ -2,7 +2,7 @@
 
 cd PrepareTemplates
 dotnet publish -c Release -r win-x64 --self-contained true /p:PublishSingleFile=true
-cp ./bin/Release/net7.0/win-x64/publish/PrepareTemplates.exe ../PrepareTemplates.exe
+cp ./bin/Release/net9.0/win-x64/publish/PrepareTemplates.exe ../PrepareTemplates.exe
 dotnet publish -c Release -r linux-x64 --self-contained true /p:PublishSingleFile=true
 cp ./bin/Release/net7.0/linux-x64/publish/PrepareTemplates ../PrepareTemplates-linux
 dotnet publish -c Release -r osx-x64 --self-contained true /p:PublishSingleFile=true
